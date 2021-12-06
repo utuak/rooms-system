70243a5a7cf9cd6864a7cb571575063055c9fb4d
utuak <95635468+utuak@users.noreply.github.com>
2021-12-06 18:15:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,13 @@
 class Room{
     player1
     player2
-    p1white
-    mess(data){
+    message(data){
         this.player1.send(data)
         this.player2.send(data)
-    }
-    raise(data, f){
-        if(f) this.player2.send(data)
-        else this.player1.send(data)
-    }
-    throwResult(ri){
-        this.player1.send("end|" + ri)
-        this.player2.send("end|" + !ri)
     }
     constructor(player1, player2, p1white) {
         this.player1 = player1
         this.player2 = player2
-        this.p1white = p1white
     }
 }
 
@@ -27,46 +17,21 @@ let waitingPlayer
 let freeRoom = false
 wss.on('connection', ws => {
     console.log("connect")
-    let socketRoom
-    let abortStep //boolean
-    let first
-    ws.on('start-game', room => {
-        socketRoom = room
-        ws.send("start|" + '|' + socketRoom.p1white + '|')
-        console.log("start")
-    })
     ws.on('message', data => {
         const arr = data.toString().split('|')
         switch (arr[0]){
             case "find":
-                if(!abortStep){
-                    console.log("find")
-                    if(!freeRoom) waitingPlayer = ws
-                    else{
-                        const currentRoom = new Room(waitingPlayer, ws, Math.random() > 0.5)
-                        waitingPlayer.emit('start-game', currentRoom)
-                        ws.emit('start-game', currentRoom)
-                        waitingPlayer = null
-                    }
-                    freeRoom = !freeRoom
-                    first = freeRoom
-                    ws.on('close', () => {
-                        console.log('disc')
-                        if(first) socketRoom.player2.send("win")
-                        else socketRoom.player1.send("win")
-                        abortStep = false
-                    })
+                if(!freeRoom) waitingPlayer = ws
+                else{
+                    const currentRoom = new Room(waitingPlayer, ws, Math.random() > 0.5)
+                    waitingPlayer.emit('start-game', currentRoom)
+                    ws.emit('start-game', currentRoom)
+                    waitingPlayer = null
                 }
-                else waitingPlayer = null
-                abortStep = !abortStep
-                break
-            case "move":
-                console.log(arr[1])
-                socketRoom.mess("move|" + arr[1])
-                break
-            case "end":
-                socketRoom.mess("draw")
-                break
+                freeRoom = !freeRoom
+                ws.on('close', () => {
+                    console.log('disc')
+                })
         }
     })
 })
